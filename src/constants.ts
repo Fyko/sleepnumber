b2b8ec939b6d2006dc8746b58ea597c3be96345a
Carter Himmel <me@fyko.net>
2022-03-31 21:28:41 -0600

    feat: iOS shortcut and instructions

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const API_BASE = 'https://prod-api.sleepiq.sleepnumber.com/rest';
 
 export const Routes = {
+	bed: `${API_BASE}/bed`,
 	login: `${API_BASE}/login`,
 	sleepNumber: (bed: string) => `${API_BASE}/bed/${bed}/sleepNumber`,
-
-};
+} as const;
